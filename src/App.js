1eef05e93f2eb5ba064f0e736bc032c124f39a55
lndsilva <lndsilva@hotmail.com>
2023-12-19 15:15:59 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Topo from "./components/Topo";
-import Principal from "./components/Principal";
+import Home from "./components/Home";
+import Frontend from "./components/Frontend";
+import Programacao from "./components/Programacao";
+import Design from "./components/Design";
+import Catalogo from "./components/Catalogo";
+import NotFound from "./components/NotFound";
 import Rodape from "./components/Rodape";
+import "./index.css";
 
-const App = () => {
-  return (
-    <>
-      <Topo />
-      <Principal />
-      <Rodape />
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Topo />
+        <Routes>
+          <Route exact path="/" element={Home} />
+          <Route exact path="/frontend" element={() => <Frontend />} />
+          <Route exact path="/programacao" element={() => <Programacao />} />
+          <Route exact path="/design" element={() => <Design />} />
+          <Route exact path="/catalogo" element={(props) => <Catalogo />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Rodape />
+      </Router>
+    );
+  }
+}
 
 export default App;
